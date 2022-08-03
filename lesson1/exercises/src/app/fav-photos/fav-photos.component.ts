84c1cb6325810664b24adaf8e67de47844d04f70
Brody Neal <brodyneal@Brody-MBP.attlocal.net>
2022-08-03 18:11:49 -0500

    Finished

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://upload.wikimedia.org/wikipedia/en/1/14/Tenet_movie_poster.jpg';
+  image3 = 'https://irs.www.warnerbros.com/keyart-jpeg/inception_keyart.jpg';
 
   constructor() { }
 
